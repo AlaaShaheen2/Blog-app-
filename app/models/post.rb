9584eafa26061703e633ba08d81791c9cec003e7
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-01-28 23:25:17 +0430

    Create custom methods for models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   belongs_to :author, class_name: 'User'
   after_save :update_posts_counter
+
+  def check_last_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
