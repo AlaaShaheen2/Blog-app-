c525f050782f418173fb4128d8a8eed0624f575e
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-01-29 03:34:14 +0430

    add tests and validations to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post, class_name: 'Post', foreign_key: :post_id
   after_save :update_comments_counter
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
   private
 
   def update_comments_counter
-    post.increment!(:comments_counter)
+    post.update(comments_counter: post.comments.length)
   end
 end
