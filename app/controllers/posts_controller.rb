925e0b46ec15cba3e22d9fd7db19c1005290b752
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-02-11 02:51:08 +0430

    add action and URL for deletion

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,6 @@ class PostsController < ApplicationController
   end
 
   def create
-    puts 'Current User'
-
     @post = Post.new(post_params)
     @post.author = current_user
 
@@ -26,6 +24,17 @@ class PostsController < ApplicationController
       else
         render :new
       end
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      redirect_to user_posts_path(current_user), notice: 'Post deleted successfully'
+    else
+      flash.new[:alert] = @post.errors.full_messages.first if @post.errors.any?
+      render :show, status: 400
     end
   end
 
