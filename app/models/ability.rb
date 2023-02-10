83f55c111798dd584ff877f0cb80fcb84f5fd769
Jhosep <jose-zromero@hotmail.com>
2023-02-10 11:28:13 -0600

    Add cancan abilityes file

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+class Ability
+    include CanCan::Ability
+  
+    def initialize(user)
+      user ||= User.new
+      if user.role == 'admin'
+        can :manage, :all
+      else
+        can :destroy, Post, author_id: user.id
+        can :destroy, Comment, author_id: user.id
+      end
+    end
+  end
