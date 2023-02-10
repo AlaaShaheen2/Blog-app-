e2f9e22d8270820ee82a827adf20d18d9952fb37
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-02-11 01:57:30 +0430

    Update users athuroties

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,16 @@
 class Ability
-    include CanCan::Ability
-  
-    def initialize(user)
-      user ||= User.new
-      if user.role == 'admin'
-        can :manage, :all
-      else
-        can :destroy, Post, author_id: user.id
-        can :destroy, Comment, author_id: user.id
-      end
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, Post, author_id: user.id
+      can :manage, Comment, author_id: user.id
     end
   end
+end
